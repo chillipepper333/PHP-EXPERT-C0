79eb5a3c6449e4f56ce9732c4b33ffd2f7357e22
Wouter Baas <Wouter.Baas@student.rocva.nl>
2021-05-26 10:43:08 +0200

    php expert 1-arrays

--- 1-Arrays/taak01 - Multidimensionale Arrays/index.php
+++ 1-Arrays/taak01 - Multidimensionale Arrays/index.php
@@ -1,1 +1,20 @@
+<?php
+$medewerkers = [
+  [
+    'voornaam' => 'Willem',
+    'achternaam' => 'van Oranje',
+    'functie'  => 'koning'
+  ],
+  [
+    'voornaam' => 'Donald',
+    'achternaam' => 'Trump',
+    'functie'  => 'president'
+  ]
+];
 
+foreach($medewerkers as $person){
+    echo $person['voornaam'] . "<br>";
+    echo $person['achternaam'] . "<br>";
+    echo $person['functie'] . "<br>";
+}
+?>
